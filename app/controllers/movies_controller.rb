bd2573444c3847f4954d6b2c5a3f98e76bd0b49c
Josh Jeong <jcjeong@gmail.com>
2014-12-07 19:20:13 -0800

    Adds movies#update route

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -10,4 +10,14 @@ class MoviesController < ApplicationController
     @movie = Movie.find(params[:id])
     respond_with @movie
   end
+
+  def update
+    @movie = Movie.find(params[:id])
+    if [true, false, "true", "false"].include?(params[:watched])
+      @movie.update_attributes(watched: params[:watched])
+    else
+      render nothing: true, status: 304 
+    end
+  end
+
 end
