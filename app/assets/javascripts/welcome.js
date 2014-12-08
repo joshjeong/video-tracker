42d82fcc657647fb1a0b559a47702389c4eb430d
Josh Jeong <jcjeong@gmail.com>
2014-12-08 14:19:48 -0800

    Moves collection view logic out of main

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -19,28 +19,6 @@ $(document).ready(function(){
         // }
     });
 
-    var MovieListView = Backbone.View.extend({
-
-        initialize: function(){
-            // Will listen for newly added movies and will call
-            // addOne function to render
-            this.collection.on('add', this.addOne, this);
-            this.collection.on('reset', this.addAll, this);
-        },
-        addOne: function(movie){
-            // each model with its own view
-            var movieView = new MovieView({model: movie});
-            $('.movies_list').append(movieView.render())
-        },
-        addAll: function(){
-            // Will call addOne and pass 'this' for correct context
-            this.collection.forEach(this.addOne, this);
-        },
-        render: function(){
-            this.addAll();
-        }
-
-    });
     var movieListView = new MovieListView({collection: movies});
 
 
