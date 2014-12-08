0f088563d85fcc9cc1f53b79173b54740a0ff401
Josh Jeong <jcjeong@gmail.com>
2014-12-08 10:53:57 -0800

    Refactors fetched method

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -10,17 +10,39 @@ $(document).ready(function(){
 
 
     movies.fetch({
-        success: function(){
-            _.each(movies.models, function(model){
-                view = new MovieView({model: model})
-                if(model.get('watched')==true){
-                    view.$el.addClass('watched')
-                }
-                var img = $('<img/>').attr({'src': model.get('image')})
-                $('.movies_list').append(view.render())
-            })
-        }
+        // success: function(){
+        //     _.each(movies.models, function(model){
+        //         view = new MovieView({model: model})
+        //     //     var img = $('<img/>').attr({'src': model.get('image')})
+        //         $('.movies_list').append(view.render())
+        //     })
+        // }
     });
+
+    var MovieListView = Backbone.View.extend({
+
+        initialize: function(){
+            // Will listen for newly added movies and will call
+            // addOne function to render
+            this.collection.on('add', this.addOne, this);
+            this.collection.on('reset', this.addAll, this);
+        },
+        addOne: function(movie){
+            // each model with its own view
+            var movieView = new MovieView({model: movie});
+            $('.movies_list').append(movieView.render())
+        },
+        addAll: function(){
+            // Will call addOne and pass 'this' for correct context
+            this.collection.forEach(this.addOne, this);
+        },
+        render: function(){
+            this.addAll();
+        }
+
+    });
+    var movieListView = new MovieListView({collection: movies});
+
 
 
 
