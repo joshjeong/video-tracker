fff327ec7abff377373a6988c90e13f76ce1354e
Josh Jeong <jcjeong@gmail.com>
2014-12-11 12:55:28 -0800

    Movie and show controller to return json on update

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -13,6 +13,7 @@ class MoviesController < ApplicationController
     @movie = Movie.find(params[:id])
     if [true, false].include?(params[:watched])
       @movie.update_attributes(watched: params[:watched])
+      respond_with @movie
     else
       render nothing: true, status: 304 
     end
